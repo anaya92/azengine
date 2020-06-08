e226c32238ce6563a3f399779bc47fd08be23fa6
Anaya Khadka <anayakhadka@icloud.com>
2020-06-08 09:43:19 -0400

    Restructure Directories + Fix Website

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@ SET COMPILER_PATH=C:\raylib\mingw\bin
 SET CC=%COMPILER_PATH%\gcc.exe
 SET CFLAGS=%RAYLIB_PATH%\src\raylib.rc.data "-IC:\Program Files (x86)\Lua\5.3\include" "-LE:\Files\programming\azragame\ext\lib" "-LC:\Program Files (x86)\Lua\5.3\lib" -w -s -static -Os -std=c99 -Iexternal -DPLATFORM_DESKTOP
 SET LDFLAGS=-lraylib -llua53 -lpthread -lopengl32 -lgdi32 -lwinmm -lasmxml
-cmd /c IF EXIST az.exe del /F az.exe
-%CC% -o az.exe *.c %CFLAGS% %LDFLAGS%
+cmd /c IF EXIST bin\az.exe del /F bin\az.exe
+%CC% -o bin/az.exe *.c %CFLAGS% %LDFLAGS%
 @echo on
